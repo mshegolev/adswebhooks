fe8c4582db9acb90f773ead8b0aea70840671926
Mikhail Shchegolev <mikhail.shchegolev@nordigy.ru>
2017-02-01 17:47:49 +0300

    change structure

--- src/com/ringcentral/adswebhooks/Main.java
+++ src/com/ringcentral/adswebhooks/Main.java
@@ -9,31 +9,43 @@ import org.jsoup.nodes.Document;
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Main {
 
-    private static int deplyment_id = 16247, port = 80;
-    private static String protocol = "http", ads_instance_name = "ads-qa.lab.nordigy.ru", path = "/api/v2/deployment/";
+    private static int deplyment_id = 16247, port = 80, port_ads = 80, port_glip= 80;
+    private static String protocol_ads = "http", instance_name_ads = "ads-qa.lab.nordigy.ru", path_ads = "/api/v2/deployment/";
+    private static String protocol_glip = "https", instance_name_glip = "hooks.glip.com",
+                            path_glip = "/webhook/", webhook_id_glip = "94ab4ac8-191a-4a8c-ad35-5664b75fb5bb";
     final String USER_AGENT = "Mozilla/5.0";
     String username = "loaduser1", password = "loaduser1";
 
     public static void main(String[] args) throws Exception {
+        send_web_hook();
         JsonElement jelement = new JsonParser().parse(getJsonDeployment(deplyment_id).toString());
         String state = jelement.getAsJsonObject().get("state").getAsString();
+        if (state.equals("Finished") || state.equals("Stopped")) System.out.println("include");;
 
-        System.out.println(state.toString());
+    }
+
+    private static void send_web_hook() throws IOException {
+
+
     }
 
 
-    static URL adsUrlDeployments() throws MalformedURLException {
-        return adsUrlDeployments(protocol, ads_instance_name, port, path);
+    static URL url_generator_ads() throws MalformedURLException {
+        return url_generator(protocol_ads, instance_name_ads, port_ads, path_ads);
+    }
+    static URL url_generator_glip() throws MalformedURLException {
+        return url_generator(protocol_glip, instance_name_glip, port_glip, path_glip);
     }
 
-    static URL adsUrlDeployments(String protocol, String ads_instance_name, int port, String path) throws MalformedURLException {
-        return new URL(protocol, ads_instance_name, port, path);
+    static URL url_generator(String protocol, String dns, int port, String path) throws MalformedURLException {
+        return new URL(protocol, dns, port, path);
     }
 
     private static String createCredentionals() {
@@ -56,16 +68,16 @@ public class Main {
     }
 
     private static String create_deployment_url() throws MalformedURLException {
-        return adsUrlDeployments().toString() + deplyment_id;
+        return url_generator_ads().toString() + deplyment_id;
+    }
+    private static String create_webhook_url() throws MalformedURLException {
+        return url_generator_glip().toString() + webhook_id_glip;
     }
 
     private void sendPost() throws Exception {
-
-        String url = "https://hooks.glip.com/webhook/94ab4ac8-191a-4a8c-ad35-5664b75fb5bb";
+        String url = create_webhook_url();
         URL obj = new URL(url);
         HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-
-        //add reuqest header
         con.setRequestMethod("POST");
         con.setRequestProperty("User-Agent", USER_AGENT);
         con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
